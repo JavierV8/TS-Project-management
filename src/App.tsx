96697b16e681d26d4cfc820ba42fdf13c3104a9c
Javier Sanchez <javier.san.mail@gmail.com>
2021-08-31 16:26:38 +0200

    added list elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import List from './components/list';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+      <div className="project-board">
+        <List />
+        <List />
+        <List />
+        <List />
+        <button className="add-list-button">Add a new list</button>
+      </div>
     </div>
   );
 }
---
+++ src/components/list.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/listStyle.css';
+
+const List = () => {
+    return (
+        <div className="list-container">
+
+        </div>
+    )
+}
+
+export default List;
