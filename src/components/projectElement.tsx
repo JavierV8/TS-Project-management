cb25345a7b38feae56d7b664c2e6099c1748af71
Javier Sanchez <javier.san.mail@gmail.com>
2021-09-01 18:03:14 +0200

    added tags at task element

--- src/components/projectElement.tsx
+++ src/components/projectElement.tsx
@@ -5,6 +5,13 @@ const ProjectElement = () => {
     return (
         <div className="project-element">
             <p className="task-title">Title of the task that has been created...</p>
+            <div className="task-tag-container">
+            <span className="task-tag">Web</span>
+            <span className="task-tag">Component</span>
+            <span className="task-tag">Component</span>
+            <span className="task-tag">Component</span>
+            </div>
+
         </div>
     )
 }
